a6872fd5596257ce64de10491a8ccc9bcf07267e
Felipe L. Melo <felipe.lufel@gmail.com>
2018-03-06 00:48:02 -0300

    Modificação da Variavel 'opcao' para STR linha 7, adicionado chaves{} e .format(opcao) linha 8, modificado linha 12 e 14 na estrutura para String

--- agenda.py
+++ agenda.py
@@ -4,14 +4,14 @@ import funcoes
 funcoes.bemvindo()
 
 #Opcoes do Usuario
-opcao = int(raw_input())
-print("Selecionou a ", opcao)
+opcao = str(input('Digite a opcao desejada: ')).strip()
+print("Selecionou a {}".format(opcao))
 
 
 #Estrutura de controle
-if opcao == 1:
+if opcao == '1':
 	funcoes.adicionar()
-elif opcao == 2:
+elif opcao == '2':
 	funcoes.listar()
 else:
 	funcoes.falha()
